5d3fc41d66a21049a928dd902cba593584187b54
princessV <puddingtea07@hotmail.com>
2017-09-06 11:51:59 -0400

    add testcases

--- netsec_fall2017/lab_1b/submission.py
+++ netsec_fall2017/lab_1b/submission.py
@@ -1,47 +1,78 @@
 import unittest
 
+from playground.network.packet.encoders.PacketEncodingError import PacketEncodingError
+from playground.network.packet import PacketType
 from mypackets import RequestMenu, Menu, Order, Result
 
 
 class MyPacketsTestCase(unittest.TestCase):
 
-    def test_RequestMenu(self):
+    def test_packetOfRequestMenu(self):
         request_menu_before = RequestMenu()
         request_menu_ser = request_menu_before.__serialize__()
         request_menu_after = RequestMenu.Deserialize(request_menu_ser)
         self.assertEqual(request_menu_before, request_menu_after)
 
-    def test_Menu(self):
+    def test_packetOfMenu(self):
         menu_before = Menu()
-
-        menu_before.ID = 1
-        menu_before.setMealA = 'A'
-        menu_before.setMealB = 'B'
-        menu_before.setMealC = 'C'
-
+        self.initPacket(menu_before, [1, 'A', 'B', 'C'])
         menu_ser = menu_before.__serialize__()
         menu_after = Menu.Deserialize(menu_ser)
         self.assertEqual(menu_before, menu_after)
 
-    def test_Order(self):
+    def test_packetOfOrder(self):
         order_before = Order()
-
-        order_before.ID = 1
-        order_before.setMeal = 'B'
-
+        self.initPacket(order_before, [1, 'B'])
         order_ser = order_before.__serialize__()
         order_after = Order.Deserialize(order_ser)
         self.assertEqual(order_before, order_after)
 
-    def test_Result(self):
+    def test_packetOfResult(self):
         result_before = Result()
-
-        result_before.ID = 1
-        result_before.price = 10
-
+        self.initPacket(result_before, [1, 10])
         result_ser = result_before.__serialize__()
         result_after = Result.Deserialize(result_ser)
         self.assertEqual(result_before, result_after)
+
+    def test_negativeUINT(self):
+        result = Result()
+        with self.assertRaises(ValueError):
+            result.ID = -1
+
+    def test_unSetValue(self):
+        result = Result()
+        result.ID = 1
+        with self.assertRaises(PacketEncodingError):
+            result.__serialize__()
+
+    def test_deserializer(self):
+        menu = Menu()
+        order = Order()
+        result = Result()
+        self.initPacket(menu, [2, 'A234', 'B323', 'C967'])
+        self.initPacket(order, [2, 'A234'])
+        self.initPacket(result, [2, 100])
+        pkt_bytes = menu.__serialize__() + order.__serialize__() + result.__serialize__()
+        deserializer = PacketType.Deserializer()
+
+        i = 0
+        while len(pkt_bytes) > 0:
+            chunk, pkt_bytes = pkt_bytes[:8], pkt_bytes[8:]
+            deserializer.update(chunk)
+            for packet in deserializer.nextPackets():
+                if i == 0:
+                    i += 1
+                    self.assertEqual(packet, menu)
+                elif i == 1:
+                    i += 1
+                    self.assertEqual(packet, order)
+                else:
+                    self.assertEqual(packet, result)
+
+    @staticmethod
+    def initPacket(packet, attr_list):
+        for index in range(len(packet.FIELDS)):
+            packet.__setattr__(packet.FIELDS[index][0], attr_list[index])
 
 
 if __name__ == '__main__':
