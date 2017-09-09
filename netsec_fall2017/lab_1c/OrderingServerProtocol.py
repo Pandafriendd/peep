fbbbe2d3dc951c395106277765d96484fb18fcee
qiyanggu <puddingtea07@hotmail.com>
2017-09-09 15:33:00 -0400

    update server protocol

--- netsec_fall2017/lab_1c/OrderingServerProtocol.py
+++ netsec_fall2017/lab_1c/OrderingServerProtocol.py
@@ -2,20 +2,66 @@ import asyncio
 
 from playground.network.packet import PacketType
 
+from ..mypackets import RequestMenu, Menu, Order, Result
+from ..mypackets import init_packet
+
 
 class OrderingServerProtocol(asyncio.Protocol):
     def __init__(self):
         self.transport = None
-        self.received = []
+        self.order_number = 0
+        self.menu_dict = self.get_menu_val()
+        self.received_message = []
 
     def connection_made(self, transport):
         self.transport = transport
         pass
 
     def data_received(self, data):
-        message = PacketType.Deserialize(data)
-        print(message)
-        self.received.append(message)
+        origin_data = None
+        deserializer = PacketType.Deserializer()
+
+        while len(data) > 0:
+            chunk, data = data[:8], data[8:]
+            deserializer.update(chunk)
+            for packet in deserializer.nextPackets():
+                print('Server received {!r}'.format(packet))
+                origin_data = packet
+                self.received_message.append(origin_data)
+
+        if isinstance(origin_data, RequestMenu):
+            print('Server receive a RequestMenu message')
+            menu = self.generate_packet_of_menu()
+            self.transport.write(menu.__serialize__())
+
+        elif isinstance(origin_data, Order):
+            print('Server receive an Order message')
+            result = self.generate_packet_of_result(origin_data.ID, origin_data.setMeal)
+            self.transport.write(result.__serialize__())
+
+        else:
+            raise ValueError('Receive incorrect packet')
 
     def connection_lost(self, exc):
         self.transport = None
+
+    def generate_packet_of_menu(self):
+        menu = Menu()
+        set_meals = [k for k in self.menu_dict.keys()]
+        init_packet(menu, [self.order_number] + set_meals)
+        self.order_number += 1
+        return menu
+
+    def generate_packet_of_result(self, packet_id, set_meal):
+        result = Result()
+        price = self.menu_dict[set_meal]
+        init_packet(result, [packet_id, price])
+        return result
+
+    @staticmethod
+    def get_menu_val():
+        return {
+            'A': 5,
+            'B': 10,
+            'C': 15
+        }
