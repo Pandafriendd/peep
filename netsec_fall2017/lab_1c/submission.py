42f55638de9a25f88263586e5ba26debf6c1ea85
qiyanggu <puddingtea07@hotmail.com>
2017-09-09 13:16:31 -0400

    update basic mock transport test

--- netsec_fall2017/lab_1c/submission.py
+++ netsec_fall2017/lab_1c/submission.py
@@ -1,12 +1,39 @@
+import asyncio
 import unittest
 
 from playground.asyncio_lib.testing import TestLoopEx
 from playground.network.testing import MockTransportToProtocol
 
+from ..mypackets import Result
+
+from .OrderingServerProtocol import OrderingServerProtocol as ServerProtocol
+from .OrderingClientProtocol import OrderingClientProtocol as ClientProtocol
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    # loop_mock = TestLoopEx()
+    # transport_mock = MockTransportToProtocol()
+
+    def test_basic_mock_transport(self):
+        sp = ServerProtocol()
+
+        result = Result()
+        self.initPacket(result, [1, 10])
+        cp = ClientProtocol(result, None)
+
+        ts = MockTransportToProtocol(sp)
+        tc = MockTransportToProtocol(cp)
+
+        sp.connection_made(tc)
+        cp.connection_made(ts)
+
+        self.assertEqual(sp.received[0], result)
+
+    @staticmethod
+    def initPacket(packet, attr_list):
+        for index in range(len(packet.FIELDS)):
+            packet.__setattr__(packet.FIELDS[index][0], attr_list[index])
 
 
 if __name__ == '__main__':
