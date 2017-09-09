257bd614f5b6ed743cd56fe09d4d7dcd93159887
qiyanggu <puddingtea07@hotmail.com>
2017-09-09 13:15:57 -0400

    update protocols

--- netsec_fall2017/lab_1c/OrderingClientProtocol.py
+++ netsec_fall2017/lab_1c/OrderingClientProtocol.py
@@ -1,16 +1,28 @@
 import asyncio
 
+from playground.network.packet import PacketType
+# from ..mypackets import RequestMenu, Menu, Order, Result
+
 
 class OrderingClientProtocol(asyncio.Protocol):
-    def __init__(self):
-        self.transport = None
+
+    def __init__(self, packet, loop):
+        if isinstance(packet, PacketType):
+            self.transport = None
+            self.packet = packet
+            self.loop = loop
+        else:
+            raise ValueError('This is not a packet')
 
     def connection_made(self, transport):
         self.transport = transport
+        transport.write(self.packet.__serialize__())
         pass
 
     def data_received(self, data):
         pass
 
     def connection_lost(self, exc):
+        print('over')
+        self.transport = None
         pass
--- netsec_fall2017/lab_1c/OrderingServerProtocol.py
+++ netsec_fall2017/lab_1c/OrderingServerProtocol.py
@@ -1,16 +1,21 @@
 import asyncio
 
+from playground.network.packet import PacketType
+
 
 class OrderingServerProtocol(asyncio.Protocol):
     def __init__(self):
         self.transport = None
+        self.received = []
 
     def connection_made(self, transport):
         self.transport = transport
         pass
 
     def data_received(self, data):
-        pass
+        message = PacketType.Deserialize(data)
+        print(message)
+        self.received.append(message)
 
     def connection_lost(self, exc):
         self.transport = None
