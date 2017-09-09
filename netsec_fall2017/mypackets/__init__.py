6bbb29267ed20d8e17662897215dd6d6e300a2cf
qiyanggu <puddingtea07@hotmail.com>
2017-09-09 15:32:36 -0400

    add init packet method in mypackets

--- netsec_fall2017/mypackets/__init__.py
+++ netsec_fall2017/mypackets/__init__.py
@@ -1,5 +1,15 @@
+from playground.network.packet import PacketType
+
 from .RequestMenu import RequestMenu
 from .Menu import Menu
 from .Order import Order
 from .Result import Result
 from .ListTest import ListTest
+
+
+def init_packet(packet, attr_list):
+    if not isinstance(packet, PacketType):
+        raise TypeError('Init an non-packet object')
+    else:
+        for index in range(len(packet.FIELDS)):
+            packet.__setattr__(packet.FIELDS[index][0], attr_list[index])
