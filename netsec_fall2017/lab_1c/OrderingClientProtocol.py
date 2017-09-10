f472f9139b151a4c9ccae598d6dac6d7f9e778f5
qiyanggu <puddingtea07@hotmail.com>
2017-09-10 13:21:01 -0400

    format print message, add interface in connect_made()

--- netsec_fall2017/lab_1c/OrderingClientProtocol.py
+++ netsec_fall2017/lab_1c/OrderingClientProtocol.py
@@ -13,9 +13,11 @@ class OrderingClientProtocol(asyncio.Protocol):
         self.transport = None
         self.received_message = []
 
-    def connection_made(self, transport):
+    def connection_made(self, transport, rm=RequestMenu()):
+        if not isinstance(rm, RequestMenu):
+            raise TypeError('Init with an incorrect packet')
         self.transport = transport
-        rm = RequestMenu()
+        print('Client sends a RequestMenu message')
         self.transport.write(rm.__serialize__())
 
     def data_received(self, data):
@@ -26,21 +28,24 @@ class OrderingClientProtocol(asyncio.Protocol):
             chunk, data = data[:8], data[8:]
             deserializer.update(chunk)
             for packet in deserializer.nextPackets():
-                print('Client received {!r}'.format(packet))
                 data_after_deserialization = packet
                 self.received_message.append(data_after_deserialization)
 
         if isinstance(data_after_deserialization, Menu):
+            print('Client receives a Menu message')
             set_meals = [
                 data_after_deserialization.setMealA,
                 data_after_deserialization.setMealB,
                 data_after_deserialization.setMealC
             ]
             order = self.generate_packet_of_order(data_after_deserialization.ID, set_meals)
+            print('Clients sends an Order message')
             self.transport.write(order.__serialize__())
 
         elif isinstance(data_after_deserialization, Result):
             pass
+        else:
+            raise TypeError('Receive incorrect packet')
 
     def connection_lost(self, exc):
         print('over')
