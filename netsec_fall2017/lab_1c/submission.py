479fc7517dd9d1f50c50e32208f7983294f80777
Qiyang Gu <puddingtea07@hotmail.com>
2017-09-13 13:58:03 -0400

    rewrite init way of protocols based on latest update of playground, add more test cases

--- netsec_fall2017/lab_1c/submission.py
+++ netsec_fall2017/lab_1c/submission.py
@@ -15,7 +15,7 @@ from .OrderingClientProtocol import OrderingClientProtocol as ClientProtocol
 """
 
 
-class MockTransportTestCase(unittest.TestCase):
+class MockTransportTestCase1(unittest.TestCase):
 
     server_protocol = None
     client_protocol = None
@@ -24,59 +24,81 @@ class MockTransportTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.server_protocol = ServerProtocol()
         cls.client_protocol = ClientProtocol()
+        st, ct = MockTransportToProtocol.CreateTransportPair(cls.server_protocol, cls.client_protocol)
+        cls.server_protocol.connection_made(st)
+        cls.client_protocol.connection_made(ct)
 
-        ts = MockTransportToProtocol(cls.server_protocol)
-        tc = MockTransportToProtocol(cls.client_protocol)
-
-        cls.server_protocol.connection_made(tc)
-        cls.client_protocol.connection_made(ts)
-
-    def test_1_valueEqual_RequestMenu(self):
+    def test_t01_valueEqual_RequestMenu(self):
         request_menu = RequestMenu()
-        MockTransportTestCase.client_protocol.send_request_menu(request_menu)
-        self.assertEqual(MockTransportTestCase.server_protocol.received_message[0], request_menu)
+        MockTransportTestCase1.client_protocol.send_request_menu(request_menu)
+        self.assertEqual(MockTransportTestCase1.server_protocol.received_message[0], request_menu)
 
-    def test_2_valueEqual_Menu(self):
+    def test_t02_valueEqual_Menu(self):
         menu = Menu()
         init_packet(menu, [0, 'A', 'B', 'C'])
-        self.assertEqual(MockTransportTestCase.client_protocol.received_message[0], menu)
+        self.assertEqual(MockTransportTestCase1.client_protocol.received_message[0], menu)
 
-    def test_3_valueEqual_Order(self):
-        menu_id = MockTransportTestCase.client_protocol.received_message[0].ID
+    def test_t03_valueEqual_Order(self):
+        menu_id = MockTransportTestCase1.client_protocol.received_message[0].ID
         order = Order()
         init_packet(order, [menu_id, 'A'])
-        MockTransportTestCase.client_protocol.send_order(order)
-        self.assertEqual(MockTransportTestCase.server_protocol.received_message[1], order)
+        MockTransportTestCase1.client_protocol.send_order(order)
+        self.assertEqual(MockTransportTestCase1.server_protocol.received_message[1], order)
 
-    def test_4_valueEqual_Result(self):
-        menu_id = MockTransportTestCase.client_protocol.received_message[0].ID
+    def test_t04_valueEqual_Result(self):
+        menu_id = MockTransportTestCase1.client_protocol.received_message[0].ID
         result = Result()
         init_packet(result, [menu_id, 5])
-        self.assertEqual(MockTransportTestCase.client_protocol.received_message[1], result)
+        self.assertEqual(MockTransportTestCase1.client_protocol.received_message[1], result)
 
-    def test_5_valueEqual_state_whenProcessEnds(self):
-        self.assertEqual(MockTransportTestCase.server_protocol.receiving_state, -1)
-        self.assertEqual(MockTransportTestCase.client_protocol.receiving_state, -1)
-        print('============= Test cases above are normal process when client interacts with server =============')
-        print('============= Test cases below are abnormal process when client interacts with server =============')
+    def test_t05_valueEqual_state_whenProcessEnds(self):
+        self.assertEqual(MockTransportTestCase1.server_protocol.receiving_state, -1)
+        self.assertEqual(MockTransportTestCase1.client_protocol.receiving_state, -1)
 
-    def test_6_exception_sendPacketAtWrongState(self):
+    def test_t06_exception_sendPacketWhenTransportCloses(self):
         rm = RequestMenu()
-        with self.assertRaises(ValueError):
-            MockTransportTestCase.client_protocol.send_request_menu(rm)
+        with self.assertRaises(AttributeError):
+            MockTransportTestCase1.client_protocol.send_request_menu(rm)
 
-    def test_7_exception_returnPacketAtWrongState(self):
-        MockTransportTestCase.server_protocol.receiving_state = 0
-        MockTransportTestCase.server_protocol.received_message = []
-        with self.assertRaises(ValueError):
-            rm = RequestMenu()
-            MockTransportTestCase.server_protocol.data_received(rm.__serialize__())
+#
+#
+#
 
-    def test_8_exception_sendingInValidPacket(self):
-        MockTransportTestCase.client_protocol.received_message = []
-        MockTransportTestCase.client_protocol.receiving_state = 0
+
+class MockTransportTestCase2(unittest.TestCase):
+
+    server_protocol = None
+    client_protocol = None
+
+    @classmethod
+    def setUpClass(cls):
+        print('--------- -------------- ---------')
+        print('--------- New Test Class ---------')
+        print('--------- -------------- ---------')
+        cls.server_protocol = ServerProtocol()
+        cls.client_protocol = ClientProtocol()
+        st, ct = MockTransportToProtocol.CreateTransportPair(cls.server_protocol, cls.client_protocol)
+        cls.server_protocol.connection_made(st)
+        cls.client_protocol.connection_made(ct)
+
+    def test_t07_valueEqual_automaticallyIncreasingID(self):
+        self.assertEqual(MockTransportTestCase2.server_protocol.ORDER_NUMBER, 1)
+
+    def test_t08_exception_sendInValidPacket(self):
         with self.assertRaises(TypeError):
-            MockTransportTestCase.client_protocol.send_request_menu('Test')
+            MockTransportTestCase2.client_protocol.send_request_menu('Test')
+
+    def test_t09_exception_sendPacketAtWrongState(self):
+        order = Order()
+        init_packet(order, [1, 'A'])
+        with self.assertRaises(ValueError):
+            MockTransportTestCase2.client_protocol.send_order(order)
+
+    def test_t10_exception_returnPacketAtWrongState(self):
+        result = Result()
+        init_packet(result, [1, 5])
+        with self.assertRaises(ValueError):
+            MockTransportTestCase2.client_protocol.data_received(result.__serialize__())
 
 
 if __name__ == '__main__':
