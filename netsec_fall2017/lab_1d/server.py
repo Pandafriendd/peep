2ac33b397637db671a57141d53580f691d34c8d4
Qiyang Gu <puddingtea07@hotmail.com>
2017-09-15 16:15:04 -0400

    add protocol stack

--- netsec_fall2017/lab_1d/server.py
+++ netsec_fall2017/lab_1d/server.py
@@ -1,6 +1,9 @@
 import asyncio
 import playground
+from playground.network.common import StackingProtocolFactory
+
 from ..lab_1c import OrderingServerProtocol
+from ..lab_1e import PassThrough1, PassThrough2
 
 '''
 
@@ -10,9 +13,13 @@ You should run by `python -m netsec_fall2017.lab_1d.server` in top level dir
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.set_debug(enabled=True)
-    sp = OrderingServerProtocol()
-    coro = playground.connect.getConnector().create_playground_server(lambda: sp, 101)
+
+    f = StackingProtocolFactory(lambda: PassThrough1(), lambda: PassThrough2())
+    ptConnector = playground.Connector(protocolStack=f)
+
+    playground.setConnector('pt', ptConnector)
+
+    coro = playground.getConnector('pt').create_playground_server(lambda: OrderingServerProtocol(), 101)
     server = loop.run_until_complete(coro)
 
     print('Serving on {}'.format(server.sockets[0].gethostname()))
