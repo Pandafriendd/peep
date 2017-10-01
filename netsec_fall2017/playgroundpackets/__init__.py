e11826c532b52c0531e900bddace379b68f35435
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-01 01:13:29 -0400

    add packet deserialize method

--- netsec_fall2017/playgroundpackets/__init__.py
+++ netsec_fall2017/playgroundpackets/__init__.py
@@ -1,1 +1,12 @@
+from playground.network.packet.PacketType import PacketType
 from .PEEPPacket import PEEPPacket
+
+
+def packet_deserialize(deserializer, data):
+    data_after_deserialization = None
+    while len(data) > 0:
+        chunk, data = data[:10], data[10:]
+        deserializer.update(chunk)
+        for packet in deserializer.nextPackets():
+            data_after_deserialization = packet
+    return data_after_deserialization
