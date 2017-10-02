274f36b2441cdaf07dfd6fa573da6b4af322c8dc
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-01 21:36:35 -0400

    update PEEP transport

--- netsec_fall2017/lab_2/PEEPTransport.py
+++ netsec_fall2017/lab_2/PEEPTransport.py
@@ -4,21 +4,4 @@ from playground.network.common import StackingTransport
 class PEEPTransport(StackingTransport):
 
     def __init__(self, lowerTransport):
-        self._protocol = None
         super().__init__(lowerTransport)
-
-    def write(self, data):
-        lt = self.lowerTransport()
-        if isinstance(lt, PEEPTransport):
-            lt.get_protocol().process_data(data)
-        else:
-            lt.write(data)
-
-    def set_protocol(self, protocol):
-        self._protocol = protocol
-
-    def get_protocol(self):
-        if self._protocol:
-            return self._protocol
-        else:
-            return None
