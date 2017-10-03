8cca947b61e0b462c1488296b3d31ef4218c0156
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-03 15:28:54 -0400

    update PEEP transport, rewrite write method

--- netsec_fall2017/lab_2/transport/PEEPTransport.py
+++ netsec_fall2017/lab_2/transport/PEEPTransport.py
@@ -1,7 +1,19 @@
 from playground.network.common import StackingTransport
 
+from ...playgroundpackets import PEEPPacket
+
 
 class PEEPTransport(StackingTransport):
 
-    def __init__(self, lowerTransport):
+    def __init__(self, lowerTransport, seq_number):
         super().__init__(lowerTransport)
+        self._protocol = None
+        self.__sequence_number_for_last_packet = seq_number
+        self._size_for_last_packet = 1
+
+    def write(self, data):
+        packet = PEEPPacket.Create_DATA(self.__sequence_number_for_last_packet, data, self._size_for_last_packet)
+        packet_bytes = packet.__serialize__()
+        self.lowerTransport().write(packet_bytes)
+        self.__sequence_number_for_last_packet = packet.SequenceNumber
+        self._size_for_last_packet = len(packet_bytes)
