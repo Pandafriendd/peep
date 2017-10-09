e6b2bb90a7b9bd4af5184746ca122ef695200321
Qiyang Gu <puddingtea07@hotmail.com>
2017-10-09 02:15:50 -0400

    remove packets-related var, make transport do transport only

--- netsec_fall2017/lab_2/transport/PEEPTransport.py
+++ netsec_fall2017/lab_2/transport/PEEPTransport.py
@@ -1,19 +1,11 @@
 from playground.network.common import StackingTransport
 
-from ...playgroundpackets import PEEPPacket
-
 
 class PEEPTransport(StackingTransport):
 
-    def __init__(self, lowerTransport, seq_number):
+    def __init__(self, lowerTransport, protocol):
         super().__init__(lowerTransport)
-        self._protocol = None
-        self.__sequence_number_for_last_packet = seq_number
-        self._size_for_last_packet = 1
+        self._protocol = protocol
 
     def write(self, data):
-        packet = PEEPPacket.Create_DATA(self.__sequence_number_for_last_packet, data, self._size_for_last_packet)
-        packet_bytes = packet.__serialize__()
-        self.lowerTransport().write(packet_bytes)
-        self.__sequence_number_for_last_packet = packet.SequenceNumber
-        self._size_for_last_packet = len(packet_bytes)
+        self._protocol.process_data(data)
