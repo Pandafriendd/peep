3c05aad464c50532e9d56fe8a509825d9286c593
Qiyang Gu <puddingtea07@hotmail.com>
2017-11-06 02:09:38 -0500

    remove unnecessary function

--- netsec_fall2017/playgroundpackets/__init__.py
+++ netsec_fall2017/playgroundpackets/__init__.py
@@ -1,11 +1,1 @@
 from .PEEPPacket import PEEPPacket
-
-
-def packet_deserialize(deserializer, data):
-    data_after_deserialization = None
-    while len(data) > 0:
-        chunk, data = data[:10], data[10:]
-        deserializer.update(chunk)
-        for packet in deserializer.nextPackets():
-            data_after_deserialization = packet
-    return data_after_deserialization
