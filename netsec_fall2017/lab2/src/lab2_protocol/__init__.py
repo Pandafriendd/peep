632ee87a21fd2e03b7325de9a3ab332678abb2a9
Qiyang Gu <puddingtea07@hotmail.com>
2017-11-06 02:08:13 -0500

    update name of protocol factory

--- netsec_fall2017/lab2/src/lab2_protocol/__init__.py
+++ netsec_fall2017/lab2/src/lab2_protocol/__init__.py
@@ -1,8 +1,8 @@
 import playground
-from ....lab_2.factory import get_client_factory, get_server_factory
+from ....lab_2.factory.PEEPFactory import get_lab2_server_factory, get_lab2_client_factory
 
-cf = get_client_factory()
-sf = get_server_factory()
+cf = get_lab2_client_factory()
+sf = get_lab2_server_factory()
 lab2_connector = playground.Connector(protocolStack=(cf, sf))
 
 playground.setConnector('lab2_protocol', lab2_connector)
