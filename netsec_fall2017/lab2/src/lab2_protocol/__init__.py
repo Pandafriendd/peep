4892cd27cb20d491a502a319ff43fe194fd13b46
Qiyang Gu <puddingtea07@hotmail.com>
2018-02-03 14:30:02 -0500

    delete depre

--- netsec_fall2017/lab2/src/lab2_protocol/__init__.py
+++
@@ -1,8 +0,0 @@
-import playground
-from ....lab_2.factory.PEEPFactory import get_lab2_server_factory, get_lab2_client_factory
-
-cf = get_lab2_client_factory()
-sf = get_lab2_server_factory()
-lab2_connector = playground.Connector(protocolStack=(cf, sf))
-
-playground.setConnector('lab2_protocol', lab2_connector)
--- netsec_fall2017/lab3/README.md
+++
@@ -1,1 +0,0 @@
-This directory is built for homework submission.
